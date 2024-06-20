fb2d95b77a7def77ee1dbf47f8732bab2e35546a
duchung128 <132693455+duchung128@users.noreply.github.com>
2024-06-20 18:42:34 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,19 +4,18 @@
 yes | pkg update && pkg upgrade
 
 # Cài đặt các gói cần thiết
-yes | pkg install libjansson wget nano git
-
-# Cài đặt git (nếu chưa cài đặt)
-pkg install git
+pkg install libjansson -y || { echo "Failed to install libjansson"; exit 1; }
+pkg install wget -y || { echo "Failed to install wget"; exit 1; }
+pkg install nano -y || { echo "Failed to install nano"; exit 1; }
+pkg install git -y || { echo "Failed to install git"; exit 1; }
 
 # Clone repository
-git clone https://github.com/duchung128/ccminer.git
+git clone https://github.com/duchung128/ccminer.git || { echo "Failed to clone repository"; exit 1; }
 
 # Sửa file .bashrc để chạy ccminer với cấu hình khi Termux khởi động
 echo '~/ccminer/ccminer -c ~/ccminer/config.json' >> ~/.bashrc
 
 # Chuyển quyền thực thi cho các file trong thư mục ccminer
-cd ccminer
-chmod +x ccminer start.sh run
-
+cd ccminer || { echo "Failed to change directory to ccminer"; exit 1; }
+chmod +x ccminer start.sh run || { echo "Failed to set executable permissions"; exit 1; }
 nano config.json
